9e64b3c970a1a7568bc2eaee62d50c06553f419c
pg9-dev <prabhat.gehlot.737@my.csun.edu>
2020-06-03 18:21:30 -0700

    Adds cors policy

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,8 @@ import { ApolloProvider } from "@apollo/react-hooks";
 
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql'
+  uri: 'http://localhost:8000/graphql',
+  credentials: 'include'
 });
 
 ReactDOM.render(
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { useLoginMutation } from "../generated/graphql";
 
 interface Props {}
 
-export const Login: React.FC<Props> = () => {
-    return <div>login</div>;
-};
+export const Login: React.FC<RouteComponentProps> = ({history}) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [login] = useLoginMutation();
+  
+    return (
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          console.log("form submitted");
+          const response = await login({
+            variables:{
+              email, 
+              password
+            }
+          });
+  
+          history.push("/");
+          console.log(response);
+        }}
+      >
+        <div>
+          <input
+            value={email}
+            placeholder="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div> 
+  
+        <div>
+          <input
+            type="password"
+            value={password}
+            placeholder="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    );
+  };
+  
