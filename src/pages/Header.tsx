4bae97d165ecf1975ae81dadabf542a7e7362879
pg9-dev <prabhat.gehlot.737@my.csun.edu>
2020-06-06 18:20:08 -0700

    Adds a logout button with the logout query

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useMeQuery } from "../generated/graphql";
+import { useMeQuery, useLogoutMutation } from "../generated/graphql";
+import { setAccessToken } from "../getAccessToken";
 
 interface Props {}
 
 export const Header: React.FC<Props> = () => {
   const { data, loading } = useMeQuery();
+  const [logout, { client }] = useLogoutMutation();
   let body: any = null;
 
   if (loading) {
@@ -30,6 +32,17 @@ export const Header: React.FC<Props> = () => {
         </div>
         <div>
           <Link to="/bye">Bye</Link>
+        </div>
+        <div>
+          {!loading && data && data.me?<button
+            onClick={async () => {
+              await logout();
+              setAccessToken("");
+              await client?.resetStore();
+            }}
+          >
+            Logout
+          </button>: null}
         </div>
         {body}
       </div>
